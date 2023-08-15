8fe155bbd9f8cb451e8e8abf91418b81d84c41f5
Kaisar <gonadotropinum@gmail.com>
2023-08-15 11:44:43 +0600

    diff_drive_controller add

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -36,6 +36,15 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+   
 
 
 
@@ -44,4 +53,5 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner
     ])
